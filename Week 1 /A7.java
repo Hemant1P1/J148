c252124de53157449f04f6f4c69e5843fd3f1b6f
Hemant1P1 <patharehemantn@gmail.com>
2025-03-02 00:24:02 +0530

    Create A7.java

---
+++ Week 1 /A7.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class A7 {
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter a character: ");
+	        char ch = sc.next().charAt(0);
+
+	        int asciiValue = (int) ch;
+
+	        System.out.println("The ASCII value of '" + ch + "' is: " + asciiValue);
+
+	        sc.close();
+	    }
+	}
+
+
+
