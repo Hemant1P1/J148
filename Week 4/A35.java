17147a555b41339e50ef28fff20c200bd8d0e351
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:21:10 +0530

    Update A35.java

--- Week 4/A35.java
+++ Week 4/A35.java
@@ -1,1 +1,29 @@
+import java.util.*;
+
+public class ToggleStringCase {
+    public static void main(String[] args) {
+        // Example string
+        String input = "javaLanguage";
+        
+        // Toggle the case of the string
+        String toggledString = toggleCase(input);
+        
+        // Print the toggled string
+        System.out.println("Toggled string: " + toggledString);
+    }
+    
+    public static String toggleCase(String str) {
+        StringBuilder result = new StringBuilder();
+        
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                result.append(Character.toLowerCase(c));
+            } else {
+                result.append(Character.toUpperCase(c));
+            }
+        }
+        
+        return result.toString();
+    }
+}
 
