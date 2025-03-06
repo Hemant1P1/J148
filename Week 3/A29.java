90bbf38ee9bbefc589b0b7413b0f18acaa6253fb
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:10:52 +0530

    Update A29.java

--- Week 3/A29.java
+++ Week 3/A29.java
@@ -1,1 +1,21 @@
+public class RemoveAdjacentDuplicates {
+    public static String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            int len = sb.length();
+            if (len > 0 && sb.charAt(len - 1) == ch) {
+                sb.deleteCharAt(len - 1);
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "abbaca";
+        String result = removeDuplicates(input);
+        System.out.println("Result: " + result); // Output: "ca"
+    }
+}
 
