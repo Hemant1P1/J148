ce3edb50dc7a1b7e29e016bb6dc071225b866e6c
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:33:07 +0530

    Update A34.java

--- Week 4/A34.java
+++ Week 4/A34.java
@@ -3,17 +3,13 @@ import java.util.*;
 public class EvenNumbersFinder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Asking user for input
         System.out.print("Enter a number (N): ");
         String number = scanner.next();
         System.out.print("Enter a digit to count (D): ");
         char digit = scanner.next().charAt(0);
         
-        // Count occurrences of digit D in number N
         int count = countDigitOccurrences(number, digit);
         
-        // Print the result
         System.out.println("The total count of digit " + digit + " occurring in the number " + number + " is: " + count);
     }
     
