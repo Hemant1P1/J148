2456ccd38a805491c5a06dd8c1db557cdd27a42a
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 11:40:02 +0530

    Update A20.java

--- Week 2/A20.java
+++ Week 2/A20.java
@@ -1,1 +1,25 @@
 
+import java.util.Scanner;
+
+public class AverageCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        
+        double[] numbers = new double[n];
+        double sum = 0;
+        
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextDouble();
+            sum += numbers[i];
+        }
+        
+        double average = sum / n;
+        System.out.println("The average is: " + average);
+        
+        scanner.close();
+    }
+}
