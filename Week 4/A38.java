293d64028455dd44734892cbb302ab47b6fed297
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:25:03 +0530

    Update A38.java

--- Week 4/A38.java
+++ Week 4/A38.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
 
+public class VowelConsonantCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+     
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine().toLowerCase();
+        
+        int vowels = 0, consonants = 0;
+        
+        String vowelsSet = "aeiou";
+        
+        for (char ch : input.toCharArray()) {
+            if (Character.isLetter(ch)) { // Check if it's a letter
+                if (vowelsSet.indexOf(ch) != -1) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+        
+       
+        System.out.println("Number of vowels: " + vowels);
+        System.out.println("Number of consonants: " + consonants);
+        
+        scanner.close();
+    }
+}
