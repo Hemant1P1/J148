b225373a7a9d84766e6ab1743cc70b0cc2c95893
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:08:39 +0530

    Update A28.java

--- Week 3/A28.java
+++ Week 3/A28.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
+
+public class DigitCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        int count = countDigits(Math.abs(number)); // Handle negative numbers
+        
+        System.out.println("Number of digits: " + count);
+        scanner.close();
+    }
+
+    public static int countDigits(int num) {
+        if (num == 0) {
+            return 1; // Special case for 0
+        }
+        
+        int count = 0;
+        while (num > 0) {
+            num /= 10;
+            count++;
+        }
+        return count;
+    }
+}
 
