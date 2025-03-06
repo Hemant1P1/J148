07ee0a6fff2e50431e5236eee46dd8cea2c515fb
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:05:01 +0530

    Update A25.java

--- Week 3/A25.java
+++ Week 3/A25.java
@@ -1,1 +1,17 @@
+import java.util.Arrays;
+
+public class AlphabeticalSort {
+    public static void main(String[] args) {
+        String[] words = {"Banana", "Apple", "Mango", "Cherry", "Grapes"};
+        
+        // Sorting the array
+        Arrays.sort(words);
+        
+        // Displaying the sorted array
+        System.out.println("Sorted Strings in Alphabetical Order:");
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
 
