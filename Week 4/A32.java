1240bb5c4d2f15011ba23c358d1391c43425b222
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:34:24 +0530

    Update A32.java

--- Week 4/A32.java
+++ Week 4/A32.java
@@ -1,11 +1,8 @@
 public class ReplaceDigits {
     public static void main(String[] args) {
         String input = "Hello123World456";
-        char replacementChar = '*'; // Character to replace digits with
-        
-        // Replace all digits with the specified character
+        char replacementChar = '*'; 
         String output = input.replaceAll("\\d", String.valueOf(replacementChar));
-        
         System.out.println("Original String: " + input);
         System.out.println("Modified String: " + output);
     }
