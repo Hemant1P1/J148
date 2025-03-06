0efc2fd59fcff381616ad1a8b5d93af87e3264e5
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 11:27:25 +0530

    Update A14.java

--- Week 2/A14.java
+++ Week 2/A14.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        int first = 0, second = 1;
+        System.out.print("Fibonacci Series up to " + n + ": ");
+        
+        while (first <= n) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
 
