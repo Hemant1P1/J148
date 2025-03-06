3b64f767f860db201cbd73334790b6239f9b07d9
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:22:36 +0530

    Update A36.java

--- Week 4/A36.java
+++ Week 4/A36.java
@@ -1,1 +1,26 @@
+import java.util.*;
+
+public class UniqueCharacters {
+    public static void main(String[] args) {
+        // Example string
+        String input = "hello world";
+        
+        // Find unique characters
+        Set<Character> uniqueChars = findUniqueCharacters(input);
+        
+        // Print the unique characters
+        System.out.println("Unique characters: " + uniqueChars);
+    }
+    
+    public static Set<Character> findUniqueCharacters(String input) {
+        Set<Character> uniqueChars = new LinkedHashSet<>();
+        
+        // Iterate over the string and add characters to the set
+        for (char c : input.toCharArray()) {
+            uniqueChars.add(c);
+        }
+        
+        return uniqueChars;
+    }
+}
 
