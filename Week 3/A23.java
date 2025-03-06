09c87c662e910996595efbec8a2aa9f7105e500f
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:38:58 +0530

    Update A23.java

--- Week 3/A23.java
+++ Week 3/A23.java
@@ -2,10 +2,7 @@
 public class CharArrayToString {
     public static void main(String[] args) {
         char[] charArray = {'H', 'e', 'l', 'l', 'o'};
-        
-        // Convert char array to String
         String str = new String(charArray);
-        
         System.out.println("Converted String: " + str);
     }
 }
