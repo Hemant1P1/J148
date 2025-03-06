66750224ab0f046868a30f4c6b0fac87754964ec
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:31:00 +0530

    Update A37.java

--- Week 4/A37.java
+++ Week 4/A37.java
@@ -1,16 +1,11 @@
 public class StringImmutableExample {
     public static void main(String[] args) {
-        // Creating a String object
+        
         String original = "Hello";
-        
-        // Trying to modify the string
         String modified = original.concat(" World");
-        
-        // Printing both strings
         System.out.println("Original String: " + original);
         System.out.println("Modified String: " + modified);
         
-        // Checking if the original string is changed
         if (original.equals("Hello")) {
             System.out.println("Strings are immutable in Java.");
         } else {
