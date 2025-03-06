d3384257c6d3e231e151418a4ff5967135777e67
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:01:50 +0530

    Update A22.java

--- Week 3/A22.java
+++ Week 3/A22.java
@@ -1,1 +1,31 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 7, 1};
+        
+        // Using built-in method
+        Arrays.sort(arr);
+        System.out.println("Sorted array using Arrays.sort(): " + Arrays.toString(arr));
+        
+        // Manual sorting (Bubble Sort)
+        int[] arr2 = {5, 2, 8, 7, 1};
+        bubbleSort(arr2);
+        System.out.println("Sorted array using Bubble Sort: " + Arrays.toString(arr2));
+    }
+    
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
 
