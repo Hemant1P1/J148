7efe5e1d302f4844852c485d6b7a74c40982fd3c
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:35:14 +0530

    Update A30.java

--- Week 3/A30.java
+++ Week 3/A30.java
@@ -5,19 +5,17 @@ public class FirstUniqueCharacter {
     public static int firstUniqChar(String s) {
         Map<Character, Integer> charCount = new HashMap<>();
         
-        // Count occurrences of each character
         for (char c : s.toCharArray()) {
             charCount.put(c, charCount.getOrDefault(c, 0) + 1);
         }
         
-        // Find the first unique character
         for (int i = 0; i < s.length(); i++) {
             if (charCount.get(s.charAt(i)) == 1) {
                 return i;
             }
         }
         
-        return -1; // No unique character found
+        return -1; 
     }
 
     public static void main(String[] args) {
