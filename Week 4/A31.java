ec249c6fc51edbb8672fc30910c211e46a0b3e8e
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:13:53 +0530

    Update A31.java

--- Week 4/A31.java
+++ Week 4/A31.java
@@ -1,1 +1,25 @@
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        
+        return Arrays.equals(sArray, tArray);
+    }
+
+    public static void main(String[] args) {
+        String s = "listen";
+        String t = "silent";
+        
+        System.out.println("Are '" + s + "' and '" + t + "' anagrams? " + isAnagram(s, t));
+    }
+}
 
