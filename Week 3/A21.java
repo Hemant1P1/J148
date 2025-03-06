7e41c54e6aab22b4b179614a2938ebbd433aba1d
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:39:43 +0530

    Update A21.java

--- Week 3/A21.java
+++ Week 3/A21.java
@@ -5,9 +5,7 @@ public class ReverseArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         System.out.println("Original Array: " + Arrays.toString(arr));
-        
         reverseArray(arr);
-        
         System.out.println("Reversed Array: " + Arrays.toString(arr));
     }
 
