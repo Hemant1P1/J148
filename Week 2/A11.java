b2a727902cd1c441c86c322db1df86b7d7547fc8
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 11:19:56 +0530

    Update A11.java

--- Week 2/A11.java
+++ Week 2/A11.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    
+    // Iterative method to calculate factorial
+    public static long factorialIterative(int n) {
+        long fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static long factorialRecursive(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorialRecursive(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+        
+        System.out.println("Factorial (Iterative): " + factorialIterative(num));
+        System.out.println("Factorial (Recursive): " + factorialRecursive(num));
+    }
+}
 
