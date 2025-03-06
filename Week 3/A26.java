ad1a29c36a345986dbdc2f5b39e3c1d164e34a19
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:06:41 +0530

    Update A26.java

--- Week 3/A26.java
+++ Week 3/A26.java
@@ -1,1 +1,33 @@
+public class HighestNumbers {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 5, 50, 30, 40};
+        findHighestNumbers(arr);
+    }
+
+    public static void findHighestNumbers(int[] arr) {
+        if (arr.length < 2) {
+            System.out.println("Array should have at least two elements.");
+            return;
+        }
+
+        int highest = Integer.MIN_VALUE;
+        int secondHighest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > highest) {
+                secondHighest = highest;
+                highest = num;
+            } else if (num > secondHighest && num != highest) {
+                secondHighest = num;
+            }
+        }
+
+        if (secondHighest == Integer.MIN_VALUE) {
+            System.out.println("No second highest value found.");
+        } else {
+            System.out.println("Highest: " + highest);
+            System.out.println("Second Highest: " + secondHighest);
+        }
+    }
+}
 
