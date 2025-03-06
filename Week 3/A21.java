84ee578449840308a6e983313efe7baccdb05728
Hemant1P1 <patharehemantn@gmail.com>
2025-03-06 12:00:24 +0530

    Update A21.java

--- Week 3/A21.java
+++ Week 3/A21.java
@@ -1,1 +1,24 @@
 
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        
+        reverseArray(arr);
+        
+        System.out.println("Reversed Array: " + Arrays.toString(arr));
+    }
+
+    public static void reverseArray(int[] array) {
+        int start = 0, end = array.length - 1;
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
